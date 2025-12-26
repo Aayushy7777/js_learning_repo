d532959960a81c5b8b3df5fe8785df54c261ff3b
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2025-12-26 12:07:09 +0000

    strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,39 @@
+// const name = "aayush"
+// const RepoCount = 50
+
+// // console.log(name + Repo + "Value");
+
+// console.log(`Hello my name is ${name} and my repo Count is ${RepoCount}`);
+
+// to declare a string
+
+const gameName = new String("Aayush")
+// console.log(gameName);
+// console.log(typeof gameName);
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+// const newString  = gameName.substring(0,4);
+// console.log(newString)
+
+// const anotherString = gameName.slice(0, -4);
+// console.log(anotherString)
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
