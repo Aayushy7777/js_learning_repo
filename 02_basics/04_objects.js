1912968008fdf8dd8598b9ac2210e069ef14e051
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2026-01-06 08:55:55 +0000

    04_objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,96 @@
+const tinderUser = {}
+
+tinderUser.name = "John Doe"
+tinderUser.age = 28
+tinderUser.isVerified = true
+tinderUser.IsLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"aayush@gmail.com",
+    fullname: {
+        Userfullname: {
+            firstname: "Aayush",
+            lastname: "Sharma"
+
+        }
+
+    }
+}
+// console.log(regularUser.fullname?.Userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = {...obj1, ...obj2}
+// const obj3 = Object.assign({}, obj1, obj2)
+
+// const obj3 = {...obj1, ...obj2}  // most used syntax
+
+// console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        email: "aayush@google.com",
+
+    },
+    {
+        id: 1,
+        email: "aayush@google.com",
+
+    },
+    {
+        id: 1,
+        email: "aayush@google.com",
+
+    },
+    {
+        id: 1,
+        email: "aayush@google.com",
+
+    },
+    {
+        id: 1,
+        email: "aayush@google.com",
+
+    },
+
+]
+// console.log(users[1].email);
+
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.IsLoggedIn.hasOwnProperty());
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
