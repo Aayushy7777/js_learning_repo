5637dda08d52e180fb4934aab7226db25eaadeba
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2026-01-06 07:23:47 +0000

    01_arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,25 +1,25 @@
 //ARRAYS
 
 const myArr = [0, 1, 2, 3, 4, 5]
-// const myHeors = ["shaktiman", "naagraj"]
+const myHeors = ["shaktiman", "naagraj"]
 
-// const myArr2 = new Array(1, 2, 3, 4)
+const myArr2 = new Array(1, 2, 3, 4)
 
-// console.log(myArr[1]);
+console.log(myArr[1]);
 
 // array methods
 
-// myArr.push(6);
-// myArr.push(7);
+myArr.push(6);
+myArr.push(7);
 
-// myArr.unshift(9);
-// myArr.shift();
+myArr.unshift(9);
+myArr.shift();
 
-// console.log(myArr);
+console.log(myArr);
 
 
-// console.log(myArr.includes(9));
-// console,log(myArr.indexOf(3));
+console.log(myArr.includes(9));
+console,log(myArr.indexOf(3));
 
 // slice, splice
 
