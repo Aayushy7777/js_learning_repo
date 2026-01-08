67fb563cac36a0bad68da8144d69b925afccc300
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2026-01-08 11:44:23 +0000

    part2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,10 +3,55 @@ let a = 300
 
 if (true) {
     let a = 10
-    const b = 20
-    console.log("INNER: ", a)
+    const b = 20                            // scope
+    // console.log("INNER: ", a)
 }
 
-console.log(a);
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+/**************************************************** */
+
+function one(){
+    const username = "aayush"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+if (true){
+    const username = "hitesh"
+    if(username == "hitesh"){
+        const website = "youtube"
+        // console.log(website+username);
+
+    }
+    // console.log(website);
+    
+
+}
+// console.log(username);
+
+one()
+
+/**************************************** */ //interesting
+
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1;
+
+}
+
+addTwo(5)
+const addTwo = function(num){            // we cant declare in this case
+    return num + 2
+
+}
