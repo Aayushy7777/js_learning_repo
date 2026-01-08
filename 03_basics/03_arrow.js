08f11c39f0a596b3e8f17c6275e0cb55cdaf26a8
Aayush Yadav <139547466+Aayushy7777@users.noreply.github.com>
2026-01-08 12:51:37 +0000

    03_arrows

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username: "Aayush",
+    price : 999,
+
+
+    WelcomeMessage : function(){
+    // console.log(` ${this.username} , welcome to the website`);
+    // console.log(this)
+
+
+}
+}
+// user.WelcomeMessage();
+// user.username = "Sam"
+// user.WelcomeMessage();
+// console.log(this)
+
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai= () => {
+//     let username = "aayush"
+//     console.log(this);
+
+
+// }
+
+// chai()
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+console.log(addTwo(3,4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
